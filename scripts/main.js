8f5a0ff26946d8845d2d3cdd11a79c4c1e224fdc
tigayalla <139618795+tigayalla@users.noreply.github.com>
2023-07-15 14:59:50 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
 
